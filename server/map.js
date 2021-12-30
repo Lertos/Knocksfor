e418c2adb0b8aa49fc9682273eeff57b0563e13f
Lertos <deetasticgaming@gmail.com>
2021-12-30 10:46:38 -0400

    Adding save/load map functionality

--- server/map.js
+++ server/map.js
@@ -1,50 +1,24 @@
+const fs = require('fs')
 
-const mapData = [
-    {
-        id: 'grass',
-        display: 'Grass',
-        file: 'grass.png',
-        occurence: 0.87
-    },
-    {
-        id: 'forest',
-        display: 'Forest',
-        file: 'forest.png',
-        occurence: 0.06
-    },
-    {
-        id: 'mountain',
-        display: 'Mountain',
-        file: 'mountain.png',
-        occurence: 0.04
-    },
-    {
-        id: 'swamp',
-        display: 'Swamp',
-        file: 'swamp.png',
-        occurence: 0.02
-    },
-    {
-        id: 'castle',
-        display: 'Castle',
-        file: 'castle.png',
-        occurence: 0.01
-    }
-]
+const mapFileName = './game_data/map.json'
+
 
 class Map {
 
-    map = []
-
+    //TODO - Might want to either change this so rows and cols aren't passed
+    ///and instead are variables above - or leave it and only generate a new map
+    //when there is no data in the save file
     constructor(rows, cols) {
         this.rows = rows
         this.cols = cols
+
+        this.map = []
 
         this.createMap()
     }
 
     createMap() {
-        mapData.sort(function (a, b) {
+        mapMetaData.sort(function (a, b) {
             return ((a.occurence > b.occurence) ? -1 : ((a.occurence == b.occurence) ? 0 : 1))
         })
 
@@ -52,38 +26,102 @@ class Map {
             let listRow = []
 
             for (let col = 0; col < this.cols; col++) {
-                listRow.push(this.getRandomTile())
+                let tile = this.getRandomTile()
+                //TODO - Add x/y tile coordinate to each map tile
+
+                listRow.push(tile)
             }
             this.map.push(listRow)
+        }
+        this.saveMap()
+        this.loadMap()
+    }
+
+    saveMap() {
+        try {
+            fs.writeFileSync(mapFileName, JSON.stringify(this.map))
+        } catch (err) {
+            console.error(err)
         }
     }
 
     loadMap() {
+        try {
+            let rawData = fs.readFileSync(mapFileName)
+            let parsed = JSON.parse(rawData)
 
+            this.map = parsed
+        } catch (err) {
+            console.error(err)
+        }
     }
 
     getMap() {
         return this.map
     }
 
-    getMapData() {
-        return mapData
+    getMapMetaData() {
+        return mapMetaData
     }
 
+    //TODO - Given a x,y coordinate (being the center - where the player is to start),
+    //have a const above that says "tileRangeToLoad" which loads tiles in all directions by that amount
+    getMapChunk() {
+
+    }
+
+    //TODO - Add ownership, random start level, growth speed double
     getRandomTile() {
         let rand = Math.random()
         let totalChance = 0
 
-        for (let tile = 0; tile < mapData.length; tile++){
-            totalChance += mapData[tile].occurence
+        for (let tile = 0; tile < mapMetaData.length; tile++){
+            totalChance += mapMetaData[tile].occurence
             
             if (rand <= totalChance) {
-                return mapData[tile]
+                return mapMetaData[tile]
             }    
         }
     }
 
 }
 
+const mapMetaData = [
+    {
+        id: 'grass',
+        display: 'Grass',
+        file: 'grass.png',
+        ownable: true,
+        occurence: 0.87
+    },
+    {
+        id: 'forest',
+        display: 'Forest',
+        file: 'forest.png',
+        ownable: true,
+        occurence: 0.06
+    },
+    {
+        id: 'mountain',
+        display: 'Mountain',
+        file: 'mountain.png',
+        ownable: true,
+        occurence: 0.04
+    },
+    {
+        id: 'swamp',
+        display: 'Swamp',
+        file: 'swamp.png',
+        ownable: true,
+        occurence: 0.02
+    },
+    {
+        id: 'castle',
+        display: 'Castle',
+        file: 'castle.png',
+        ownable: true,
+        occurence: 0.01
+    }
+]
 
 module.exports = Map
